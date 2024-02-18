efe871021ae136a2fd05ea0ebd494a251bd01215
Jefferson Cleyson Gomes Almeida <jefferson.almeida@sensedia.com>
2024-02-18 20:01:08 -0300

    chore: Adicionado 'Environment' para manipular tópicos do sns: 'SendSnsEmailEventAdapter' e 'SendSnsUpdateOrderMessageAdapter'

--- totem-food-payment-framework/src/main/java/com/totem/food/framework/adapters/out/sns/SendSnsEmailEventAdapter.java
+++ totem-food-payment-framework/src/main/java/com/totem/food/framework/adapters/out/sns/SendSnsEmailEventAdapter.java
@@ -3,20 +3,25 @@ package com.totem.food.framework.adapters.out.sns;
 import com.totem.food.application.ports.out.email.EmailNotificationDto;
 import com.totem.food.application.ports.out.event.ISendEventPort;
 import io.awspring.cloud.sns.core.SnsTemplate;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-@AllArgsConstructor
 @Component
 @Slf4j
-public class SendEmailEventAdapter implements ISendEventPort<EmailNotificationDto, Boolean> {
+public class SendSnsEmailEventAdapter implements ISendEventPort<EmailNotificationDto, Boolean> {
 
     private final SnsTemplate snsTemplate;
+    private final String emailTopicSns;
+
+    public SendSnsEmailEventAdapter(Environment env, SnsTemplate snsTemplate) {
+        this.emailTopicSns = env.getProperty("ms.internal.topic.email");
+        this.snsTemplate = snsTemplate;
+    }
 
     @Override
     public Boolean sendMessage(EmailNotificationDto item) {
-        snsTemplate.convertAndSend("email-topic", item);
+        snsTemplate.convertAndSend(this.emailTopicSns, item);
         return Boolean.TRUE;
     }
 }
--- totem-food-payment-framework/src/main/java/com/totem/food/framework/adapters/out/sns/SendSnsUpdateOrderMessageAdapter.java
+++ totem-food-payment-framework/src/main/java/com/totem/food/framework/adapters/out/sns/SendSnsUpdateOrderMessageAdapter.java
@@ -3,23 +3,28 @@ package com.totem.food.framework.adapters.out.sns;
 import com.totem.food.application.ports.in.dtos.event.PaymentEventMessageDto;
 import com.totem.food.application.ports.out.event.ISendEventPort;
 import io.awspring.cloud.sns.core.SnsTemplate;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-@AllArgsConstructor
 @Component
 @Slf4j
-public class SendSnsMessageAdapter implements ISendEventPort<PaymentEventMessageDto, Boolean> {
+public class SendSnsUpdateOrderMessageAdapter implements ISendEventPort<PaymentEventMessageDto, Boolean> {
 
     private final SnsTemplate snsTemplate;
+    private final String paymentTopicSns;
+
+    public SendSnsUpdateOrderMessageAdapter(Environment env, SnsTemplate snsTemplate) {
+        this.paymentTopicSns = env.getProperty("ms.internal.topic.payment");
+        this.snsTemplate = snsTemplate;
+    }
 
     @Override
     public Boolean sendMessage(PaymentEventMessageDto item) {
         Map<String, Object> headerAttributes = Map.of("order", "UPDATE");
-        snsTemplate.convertAndSend("payment-topic", item, headerAttributes);
+        snsTemplate.convertAndSend(this.paymentTopicSns, item, headerAttributes);
         return Boolean.TRUE;
     }
 }
